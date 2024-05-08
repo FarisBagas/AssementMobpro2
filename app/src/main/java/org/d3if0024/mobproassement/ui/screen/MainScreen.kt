451c9709badd3963643234641de00ada5631dcfc
Faris Bagas Pratama <Neocrmencer@gmail.com>
2024-05-09 05:21:34 +0700

    Membuat tampilan list

--- app/src/main/java/org/d3if0024/mobproassement/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0024/mobproassement/ui/screen/MainScreen.kt
@@ -1,9 +1,15 @@
 package org.d3if0024.mobproassement.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,14 +19,19 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if0024.mobproassement.R
+import org.d3if0024.mobproassement.model.Pesanan
 import org.d3if0024.mobproassement.ui.theme.MobproAssesment2Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+
     Scaffold (
         topBar = {
             TopAppBar(title = {
@@ -40,13 +51,50 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier){
-    Column (
+    val viewModel : MainViewModel = viewModel()
+    val data =  viewModel.data
+
+    LazyColumn (
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
     ){
+       items(data){
+           ListItem(pesanan = it){
+
+           }
+           Divider()
+       }
+    }
+}
+
+@Composable
+fun ListItem(pesanan: Pesanan, onClick: () -> Unit ){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = pesanan.size,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = pesanan.topping,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+        )
+        Text(
+            text = pesanan.drink,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
 
     }
+
 }
 
 @Preview(showBackground = true)
