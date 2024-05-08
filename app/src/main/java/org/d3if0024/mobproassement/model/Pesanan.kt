d5f68cb3ef206027b5b2a8f167b1dc81be08d124
Faris Bagas Pratama <Neocrmencer@gmail.com>
2024-05-09 05:10:53 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/org/d3if0024/mobproassement/model/Pesanan.kt
@@ -0,0 +1,8 @@
+package org.d3if0024.mobproassement.model
+
+data class Pesanan(
+    val id: Long,
+    val size: String,
+    val topping: String,
+    val drink: String
+)
---
+++ app/src/main/java/org/d3if0024/mobproassement/ui/screen/MainViewModel.kt
@@ -0,0 +1,23 @@
+package org.d3if0024.mobproassement.ui.screen
+
+import androidx.lifecycle.ViewModel
+import org.d3if0024.mobproassement.model.Pesanan
+
+    class MainViewModel : ViewModel() {
+
+        val data = getDataDummy()
+        private  fun getDataDummy(): List<Pesanan>{
+            val data = mutableListOf<Pesanan>()
+            for (i in 29 downTo 20){
+                data.add(
+                    Pesanan(
+                        i.toLong(),
+                        "Reguler",
+                        "Smoke Beef",
+                        "Soda"
+                    )
+                )
+            }
+            return data
+        }
+    }
